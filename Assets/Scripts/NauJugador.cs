7960d95473a5f69a793c8c06c8c24def7b2525f9
Lucas_Quintana <lucasquintana@ceroca.cat>
2024-09-23 16:21:56 +0200

    Limit camera

--- Assets/Scripts/NauJugador.cs
+++ Assets/Scripts/NauJugador.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class NauJugador : MonoBehaviour
 {
     private float _vel;
-
+    Vector2 minpantalla, maxpantalla;
 
     // Start is called before the first frame update
     void Start()
     {
-        _vel = 8;        
+        _vel = 8;       
+        minpantalla = Camera.main.ViewportToWorldPoint(new Vector2(0,0));
+        maxpantalla = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+
+        minpantalla.x = minpantalla.x + 0.5f;
+        maxpantalla.x = maxpantalla.x + 0.5f;
+        minpantalla.x = maxpantalla.x + 0.5f;
+
     }
 
     // Update is called once per frame
@@ -20,9 +27,14 @@ public class NauJugador : MonoBehaviour
         float direccioindicadaY = Input.GetAxisRaw("Vertical");
 
         Debug.Log("X:"+direccioindicadaX+ " Y: "+direccioindicadaY);
-        Vector2 direccioindicada = new Vector2(direccioindicadaX, direccioindicadaY);
+        Vector2 direccioindicada = new Vector2(direccioindicadaX, direccioindicadaY).normalized;
         Vector2 novapos = transform.position;
-        novapos = novapos + direccioindicada * _vel * Time.deltaTime;      
+        novapos = novapos + direccioindicada * _vel * Time.deltaTime;
+        novapos.x = Mathf.Clamp(novapos.x, minpantalla.x, maxpantalla.x);
+        novapos.x = Mathf.Clamp(novapos.y, minpantalla.y, maxpantalla.y);
+
         transform.position = novapos;
+
+
     }
 }
